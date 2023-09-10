e8e0b9019aeeea266c6e7b5a9a5ab2a6572c3037
arvindpndit <arvindpandit9808@gmail.com>
2023-09-10 09:15:22 +0530

    added the authentication middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const rateLimit = require("express-rate-limit");
+const axios = require("axios");
 
 const PORT = 3005;
 
@@ -15,6 +16,31 @@ const limiter = rateLimit({
 
 app.use(morgan("combined"));
 app.use(limiter);
+
+app.use("/bookingservice", async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      "http://localhost:3001/api/v1/isauthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
+    console.log(response.data);
+    if (response.data.success) {
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Unauthorised",
+      });
+    }
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorised",
+    });
+  }
+});
 
 app.use(
   "/bookingservice",
