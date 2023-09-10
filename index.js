a2848d1050c225ae614e439e22c03f79784cdc17
arvindpndit <arvindpandit9808@gmail.com>
2023-09-10 08:29:28 +0530

    rate limiter added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
 
 const PORT = 3005;
 
 const app = express();
 
+//max 5 request coming from an IP in 2 minutes
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 5,
+});
+
 app.use(morgan("combined"));
+app.use(limiter);
 
 app.use(
   "/bookingservice",
