ea840a4a2cafa043f2207fe89911ef2d0ce6ce2e
arvindpndit <arvindpandit9808@gmail.com>
2023-09-10 08:17:35 +0530

    added proxy middleware

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const PORT = 3005;
 
 const app = express();
 
-app.listen(3005, () => {
-  console.log("app started at 3005");
+app.use(morgan("combined"));
+
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
+);
+
+app.get("/home", (req, res) => {
+  return res.json({
+    message: "OK",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started @ ${PORT}`);
 });
